5a153261fc1f3eda6780c906fbeca4e5d7b72624
MD. SAIF ISLAM <muhammadsaif7717@gmail.com>
2024-11-19 23:02:52 +0600

    student module added

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
+import { studentRoutes } from './app/modules/student/student.route';
 
 const app: Application = express();
 
@@ -7,10 +8,11 @@ const app: Application = express();
 app.use(express.json());
 app.use(cors());
 
+// application routes
+app.use('/api/v1/students', studentRoutes)
+
 app.get('/', (req: Request, res: Response) => {
-  const a = 10;
-  console.log(`sthtrsa`);
-  res.send(a);
+  res.send(`Server is running`);
 });
 
 export default app;
